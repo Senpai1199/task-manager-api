2022107471509ecccb33a960591f7bdf2724c31b
Divyansh <divyansh1199@gmail.com>
2020-06-09 14:26:39 +0530

    Indentation fix in user models

--- src/models/user.js
+++ src/models/user.js
@@ -72,7 +72,7 @@ userSchema.methods.generateAuthToken = async function () {
   // valid for instances
   const user = this;
   const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET, {
-    expiresIn: "5     minute",
+    expiresIn: "5 minute",
   });
 
   user.tokens = user.tokens.concat({ token });
